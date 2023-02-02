d60cdb2a84482155334759f877a9ebc94e41c56a
Hinal Ghelani <121785230+HinalGhelani@users.noreply.github.com>
2023-02-02 11:21:58 +0530

    Update GlobalVar.dart

--- lib/GlobalVar.dart
+++ lib/GlobalVar.dart
@@ -4,72 +4,7 @@ import 'package:image_picker/image_picker.dart';
 
 class GlobalVar {
 
-  // static List name = [
-  //   'Hinal',
-  //   'ManishBhai',
-  //   'Dipikaben',
-  //   'Tirth',
-  //   'Reetu',
-  //   'Meet',
-  //   'Hareshbhai',
-  //   'Shilpaben',
-  //   'Kashiben',
-  //   'Ranchhodbhai'
-  // ];
-
-  static List<Map<String, dynamic>> contact = [
-    // {
-    //   'id': '1',
-    //   'name': 'Hinal Ghelani',
-    //   'number': "9876543218",
-    //   'image': "9876543218",
-    // },
-    // {
-    //   'id': '2',
-    //   'name': 'Manish Ghelani',
-    //   'number': "9876543218",
-    // },
-    // {
-    //   'id': '3',
-    //   'name': 'Tirth Ghelani',
-    //   'number': "9876543218",
-    // },
-    // {
-    //   'id': '4',
-    //   'name': 'Dipika Ghelani',
-    //   'number': "9876543218",
-    // },
-    // {
-    //   'id': '5',
-    //   'name': 'Rohit Sharma',
-    //   'number': "9876543218",
-    // },
-    // {
-    //   'id': '6',
-    //   'name': 'Rohit Sharma',
-    //   'number': "9876543218",
-    // },
-    // {
-    //   'id': '7',
-    //   'name': 'Rohit Sharma',
-    //   'number': "9876543218",
-    // },
-    // {
-    //   'id': '8',
-    //   'name': 'Rohit Sharma',
-    //   'number': "9876543218",
-    // },
-    // {
-    //   'id': '9',
-    //   'name': 'Rohit Sharma',
-    //   'number': "9876543218",
-    // },
-    // {
-    //   'id': '10',
-    //   'name': 'Rohit Sharma',
-    //   'number': "9876543218",
-    // },
-  ];
+  static List<Map<String, dynamic>> contact = [];
 
   static GlobalKey<FormState> detail = GlobalKey<FormState>();
   static TextEditingController nameF = TextEditingController();
